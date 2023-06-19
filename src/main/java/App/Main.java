68a7d98a3891d97ffeb8ed8794ebd55f2ce2e405
DmitryEvteev <dekenrig@gmail.com>
2023-06-19 20:57:56 +0300

    version 1.0

---
+++ src/main/java/App/Main.java
@@ -0,0 +1,13 @@
+package App;
+
+import src.main.java.edu.oreily.IoT;
+
+public class Main {
+    public static void main(String[] args) {
+        IoT x = new IoT();
+        x.in();
+        x.out();
+        System.out.println();
+        System.out.println("Hello world!");
+    }
+}
